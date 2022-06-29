2474af0e10e1696fa66e340056e62f3e1e4c2d2c
vinferno <vinsonfernandez@gmail.com>
2022-06-28 20:11:43 -0700

    added syntax walkthrough

--- js/main.js
+++ js/main.js
@@ -1,5 +1,30 @@
-document.querySelector('h1').innerHTML = 'Hello World';
-document.querySelector('p').innerHTML = 'Hello World';
+// comment: one line comment
+/* 
+ this is a multi-line comment
+*/ // ends with */
 
 // variables
-name = 'John';
+
+
+
+yes = true; // boolean
+no = false;
+Yes = 10;
+console.log(yes, Yes);
+
+count = 0; // number
+one = 1;
+two = 2;
+three = 3;
+
+title = 'Hello World'; // string
+
+document.querySelector('h1').innerHTML = title;
+document.querySelector('p').innerHTML = count;
+
+console.log(title);
+
+
+
+
+
