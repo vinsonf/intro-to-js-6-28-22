01188016177ff15227caf63b7b956175a7015239
vinferno <vinsonfernandez@gmail.com>
2022-06-30 19:40:21 -0700

    push

--- day02/js/main.js
+++ day02/js/main.js
@@ -1,2 +1,35 @@
-console.log(123)
-document.querySelector('h1').innerHTML = 'Hello Earth';
+// syntax
+
+day = 2;
+Day = 1;
+/* multi-line comment
+    console.log('does nto run)
+    console.log(123, day, Day);
+*/
+console.log("works again");
+document.querySelector("h1").innerHTML = "Hello Earth";
+
+
+// data types // all the answers to the questions
+
+// primitive data types
+// yes no = true false = boolean
+// 1 = number = count
+// "hello" = string = title
+
+const no = false;
+let count = 0;
+const title = "Hello World";
+
+// organized data types
+// array = [1, 2, 3, 4, 5] | list = [1, 2, 3, 4, 5]
+
+const favoriteMovie = "Back to the Future";
+let favoriteMovies = ["Back to the Future", "The Matrix", "The Matrix Reloaded", "The Matrix Revolutions"];
+
+const backToTheFuture = {
+    title: "Back to the Future",
+    director: "Robert Zemeckis",
+    year: "1985",
+    cast: ["Michael J. Fox", "Christopher Lloyd", "Lea Thompson"],
+};
