0a5d19475c0d8751282ddf2435c5f92c983b6e9a
vinferno <vinsonfernandez@gmail.com>
2022-07-12 21:04:40 -0700

    flappy

--- day05/js/main.js
+++ day05/js/main.js
@@ -42,3 +42,23 @@ button.addEventListener('click', function whatToDoWhenButtonIsClicked() {
     document.querySelector('h1').innerHTML = user.greeting();
     document.querySelector('h1').style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
 });
+
+const birdImg = document.querySelector('img');
+birdImg.style.position = 'absolute';
+birdImg.style.top = '100px';
+let gravity = 3;
+
+let gameloop = function(){}
+function gameLoop() {
+    const birdTop  = parseInt(birdImg.style.top);
+    if (gravity < 3){
+        gravity += 0.1;
+    }
+    birdImg.style.top = (birdTop + gravity) + 'px';
+    requestAnimationFrame(gameLoop);
+}
+gameLoop();
+document.addEventListener('click', function(){
+   gravity = -5;
+});
+
